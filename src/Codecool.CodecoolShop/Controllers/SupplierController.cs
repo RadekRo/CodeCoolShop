c524d9a869640239af715ce29459b4e802095981
Woleen <94767262+Woleen@users.noreply.github.com>
2023-09-15 20:52:22 +0200

    Added backend for suppliers products/ not an atomic commit for sure

---
+++ src/Codecool.CodecoolShop/Controllers/SupplierController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Codecool.CodecoolShop.Daos;
+using Codecool.CodecoolShop.Daos.Implementations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Codecool.CodecoolShop.Models;
+using Codecool.CodecoolShop.Services;
+
+namespace Codecool.CodecoolShop.Controllers
+{
+    [Route("supplier")]
+    public class SupplierController : Controller
+    {
+        private readonly ILogger<SupplierController> _logger;
+        public SupplierService SupplierService { get; set; }
+
+        public SupplierController(ILogger<SupplierController> logger)
+        {
+            _logger = logger;
+            SupplierService = new SupplierService(
+                ProductDaoMemory.GetInstance(),
+                SupplierDaoMemory.GetInstance());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Index(int id)
+        {
+            var products = SupplierService.GetProductsForSupplier(id);
+            return View(products.ToList());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

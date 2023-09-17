e41629e56386d529e7e742565dbbc83d3aa256b7
RadekRo <r.roclawski@ankara.pl>
2023-09-17 19:21:47 +0200

    Added a DAO for shopping cart

---
+++ src/Codecool.CodecoolShop/Daos/ICartDao.cs
@@ -0,0 +1,13 @@
+﻿using Codecool.CodecoolShop.Models;
+using System.Collections.Generic;
+
+namespace Codecool.CodecoolShop.Daos
+{
+    public interface ICartDao : IDao<Product>
+    {
+        void AddToCart(Product product, int qty);
+        void RemoveFromCart(Product product, int qty);
+        IEnumerable<Product> GetCartContents();
+        decimal CalculateTotalPrice();
+    }
+}

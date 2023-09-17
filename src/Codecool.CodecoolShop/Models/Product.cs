671ff03ee12bf50ff453d17ff145c8cfb15f5aff
Mateusz Miałszygrosz <118621056+MatiMMMM@users.noreply.github.com>
2023-09-17 19:03:06 +0200

    Fixed errors
    Changes in Product.cs and Index.cshtml
    Added NIndex.cshtml

--- src/Codecool.CodecoolShop/Models/Product.cs
+++ src/Codecool.CodecoolShop/Models/Product.cs
@@ -1,20 +1,47 @@
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Codecool.CodecoolShop.Models
 {
     public class Product : BaseModel
     {
-        internal object ProductCategories;
+        internal ProductCategory ProductCategory;
 
         public string Currency { get; set; }
         public decimal DefaultPrice { get; set; }
-        public ProductCategory ProductCategory { get; set; }
-        public Supplier Supplier { get; set; }
+        public HashSet<ProductCategory> ProductCategories { get; set; }
 
-        public void SetProductCategory(ProductCategory productCategory)
+        public Supplier Supplier { get; set; }
+        public int Quantity { get; set; }
+
+        public Product()
         {
-            ProductCategory = productCategory;
-            ProductCategory.Products.Add(this);
+            ProductCategories = new();
+        }
+
+
+        public void SetProductCategory(HashSet<ProductCategory> productCategories)
+        {
+            foreach (ProductCategory pc in productCategories)
+            {
+                pc.Products.Add(this);
+            }
+        }
+
+        public string PriceToString()
+        {
+            return DefaultPrice.ToString() + " " + Currency;
+        }
+
+        public string CategoryToString(HashSet<ProductCategory> productCategories)
+        {
+            List<string> str = new List<string>();
+            foreach (ProductCategory pc in productCategories)
+            {
+                str.Add(pc.Name);
+            }
+            return String.Join(", ", str);
         }
     }
 }

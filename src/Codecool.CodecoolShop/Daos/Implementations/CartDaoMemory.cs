4a5027eb0c04fb41ca8e32a1ad05cf258c7f5600
RadekRo <r.roclawski@ankara.pl>
2023-09-17 19:27:35 +0200

    Added ICartDao implementation in CartDaoMemory

---
+++ src/Codecool.CodecoolShop/Daos/Implementations/CartDaoMemory.cs
@@ -0,0 +1,48 @@
+﻿using Codecool.CodecoolShop.Models;
+using System.Collections.Generic;
+
+namespace Codecool.CodecoolShop.Daos.Implementations
+{
+    public class CartDaoMemory : ICartDao
+    {
+        public void Add(Product item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddToCart(Product product, int qty)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public decimal CalculateTotalPrice()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetCartContents()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveFromCart(Product product, int qty)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

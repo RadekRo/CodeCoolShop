9a4656718993c20038ae013bee6d7ddb6463d6f5
Adrian Paszek <adrian.paszek@gmail.com>
2023-09-21 20:20:17 +0200

    produkty kategorie i wyswietlanie

--- src/Codecool.CodecoolShop/Startup.cs
+++ src/Codecool.CodecoolShop/Startup.cs
@@ -65,15 +65,24 @@ namespace Codecool.CodecoolShop
             IProductCategoryDao productCategoryDataStore = ProductCategoryDaoMemory.GetInstance();
             ISupplierDao supplierDataStore = SupplierDaoMemory.GetInstance();
 
-            Supplier amazon = new Supplier{Name = "Amazon", Description = "Digital content and services"};
-            supplierDataStore.Add(amazon);
-            Supplier lenovo = new Supplier{Name = "Lenovo", Description = "Computers"};
-            supplierDataStore.Add(lenovo);
-            ProductCategory tablet = new ProductCategory {Name = "Tablet", Department = "Hardware", Description = "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display." };
-            productCategoryDataStore.Add(tablet);
-            productDataStore.Add(new Product { Name = "Amazon Fire", DefaultPrice = 49.9m, Currency = "USD", Description = "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", ProductCategory = tablet, Supplier = amazon });
-            productDataStore.Add(new Product { Name = "Lenovo IdeaPad Miix 700", DefaultPrice = 479.0m, Currency = "USD", Description = "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", ProductCategory = tablet, Supplier = lenovo });
-            productDataStore.Add(new Product { Name = "Amazon Fire HD 8", DefaultPrice = 89.0m, Currency = "USD", Description = "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", ProductCategory = tablet, Supplier = amazon });
+            Supplier bell = new Supplier{Name = "Bell", Description = "Kaski"};
+            supplierDataStore.Add(bell);
+            Supplier imx = new Supplier{Name = "Imx", Description = "Kaski"};
+            supplierDataStore.Add(imx);
+            Supplier hjc = new Supplier { Name = "HJC", Description = "Kaski" };
+            supplierDataStore.Add(hjc);
+            ProductCategory helmet = new ProductCategory {Name = "Kaski", Department = "Ochraniacze", Description = "Kaski" };
+            productCategoryDataStore.Add(helmet);
+            ProductCategory jackets = new ProductCategory { Name = "Kurtki", Department = "Odzie¿", Description = "Odzie¿ i ochraniacze" };
+            productCategoryDataStore.Add(jackets);
+            ProductCategory pants = new ProductCategory { Name = "Spodnie", Department = "Odzie¿", Description = "Odzie¿ i ochraniacze" };
+            productCategoryDataStore.Add(pants);
+            ProductCategory googgles = new ProductCategory { Name = "Gogle", Department = "Ochaniacze", Description = "Gogle" };
+            productCategoryDataStore.Add(googgles);
+            productDataStore.Add(new Product { Name = "IMX FMX 02", DefaultPrice = 99.9m, Currency = "USD", Description = "Kask motocyklowy do jazdy na cross IMX FM-02\r\nKask motocyklowy w którym znajdziesz wydajny system wentylacyjny, wyjmowane poduszki policzkowe i zapiêcie mikrometryczne. Skorupa tego modelu zosta³a wykonana z najwy¿szej jakoœci materia³ów ABS.\r\n\r\nHomologacja ECE 22.05\r\nSkorupa ABS\r\nZapiêcie mikrometryczne\r\n2 wielkoœci EPS\r\nAwaryjne wypiêcie poduszek policzkowych", ProductCategory = helmet, Supplier = imx });
+            productDataStore.Add(new Product { Name = "Lenovo IdeaPad Miix 700", DefaultPrice = 479.0m, Currency = "USD", Description = "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", ProductCategory = helmet, Supplier = imx });
+            productDataStore.Add(new Product { Name = "Amazon Fire HD 8", DefaultPrice = 89.0m, Currency = "USD", Description = "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", ProductCategory = helmet, Supplier = bell });
+            productDataStore.Add(new Product { Name = "zjebanyKask", DefaultPrice = 89.0m, Currency = "USD", Description = "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", ProductCategory = helmet, Supplier = bell });
         }
     }
 }

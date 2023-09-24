541003a3e42e55dd604979a1c429b83fac1dfb54
Woleen <94767262+Woleen@users.noreply.github.com>
2023-09-24 11:53:01 +0200

    Updated cart view and items to display

--- src/Codecool.CodecoolShop/Startup.cs
+++ src/Codecool.CodecoolShop/Startup.cs
@@ -68,31 +68,33 @@ namespace Codecool.CodecoolShop
             IProductCategoryDao productCategoryDataStore = ProductCategoryDaoMemory.GetInstance();
             ISupplierDao supplierDataStore = SupplierDaoMemory.GetInstance();
 
-            Supplier bell = new Supplier{Name = "Bell", Description = "Kaski"};
+            Supplier bell = new Supplier{Name = "Bell", Description = "Helmets"};
             supplierDataStore.Add(bell);
-            Supplier imx = new Supplier{Name = "Imx", Description = "Kaski"};
+            Supplier imx = new Supplier{Name = "Imx", Description = "Helmets"};
             supplierDataStore.Add(imx);
-            Supplier hjc = new Supplier { Name = "HJC", Description = "Kaski" };
+            Supplier hjc = new Supplier { Name = "HJC", Description = "Helmets" };
             supplierDataStore.Add(hjc);
-            Supplier broger = new Supplier { Name = "BROGER", Description = "Kurtki" };
+            Supplier broger = new Supplier { Name = "BROGER", Description = "Jackets" };
             supplierDataStore.Add(broger);
-            Supplier giro = new Supplier { Name = "GIRO", Description = "Spodnie" };
+            Supplier giro = new Supplier { Name = "GIRO", Description = "Pants" };
             supplierDataStore.Add(giro);
-            ProductCategory helmet = new ProductCategory {Name = "Kaski", Department = "Ochraniacze", Description = "Kaski" };
+            Supplier gogler = new Supplier { Name = "Gogler", Description = "Gogles" };
+            supplierDataStore.Add(gogler);
+            ProductCategory helmet = new ProductCategory {Name = "Helmets", Department = "Protectors", Description = "Kaski" };
             productCategoryDataStore.Add(helmet);
-            ProductCategory jackets = new ProductCategory { Name = "Kurtki", Department = "Odzie¿", Description = "Odzie¿ i ochraniacze" };
+            ProductCategory jackets = new ProductCategory { Name = "Jackets", Department = "Clothes", Description = "Clothing and protectors." };
             productCategoryDataStore.Add(jackets);
-            ProductCategory pants = new ProductCategory { Name = "Spodnie", Department = "Odzie¿", Description = "Odzie¿ i ochraniacze" };
+            ProductCategory pants = new ProductCategory { Name = "Pants", Department = "Clothes", Description = "Clothing and protectors." };
             productCategoryDataStore.Add(pants);
-            ProductCategory googgles = new ProductCategory { Name = "Gogle", Department = "Ochaniacze", Description = "Gogle" };
+            ProductCategory googgles = new ProductCategory { Name = "Gogle", Department = "Protectors", Description = "Gogle" };
             productCategoryDataStore.Add(googgles);
-            productDataStore.Add(new Product { Name = "IMX FMX 02", DefaultPrice = 99.9m, Currency = "USD", Description = "Kask motocyklowy do jazdy na cross IMX FM-02\r\nKask motocyklowy w którym znajdziesz wydajny system wentylacyjny, wyjmowane poduszki policzkowe i zapiêcie mikrometryczne. Skorupa tego modelu zosta³a wykonana z najwy¿szej jakoœci materia³ów ABS.\r\n\r\nHomologacja ECE 22.05\r\nSkorupa ABS\r\nZapiêcie mikrometryczne\r\n2 wielkoœci EPS\r\nAwaryjne wypiêcie poduszek policzkowych", ProductCategory = helmet, Supplier = imx });
-            productDataStore.Add(new Product { Name = "hjc test", DefaultPrice = 179.0m, Currency = "USD", Description = "Kask", ProductCategory = helmet, Supplier = hjc });
-            productDataStore.Add(new Product { Name = "broozer", DefaultPrice = 479.0m, Currency = "USD", Description = "Kask szczêkowy BELL BROOZER SOLID MATTE BLACK czarny", ProductCategory = helmet, Supplier = bell });
-            productDataStore.Add(new Product { Name = "MONTANA", DefaultPrice = 89.0m, Currency = "USD", Description = " Kurtka tekstylna BROGER MONTANA BLACK czarny", ProductCategory = jackets, Supplier = broger });
-            productDataStore.Add(new Product { Name = "Kask", DefaultPrice = 89.0m, Currency = "USD", Description = "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", ProductCategory = helmet, Supplier = bell });
-            productDataStore.Add(new Product { Name = "giro", DefaultPrice = 89.0m, Currency = "USD", Description = " Taktyczne bojówki w bezpiecznym, motocyklowym wydaniu to model spodni Shima Giro 2.0\r\n", ProductCategory = pants, Supplier = giro });
-
+            productDataStore.Add(new Product { Name = "IMX FMX 02", DefaultPrice = 99.9m, Currency = "USD", Description = "Motorcycle helmet for motocross IMX FM-02 A motorcycle helmet that features an efficient ventilation system, removable cheek pads, and a micrometric fastening system. The shell of this model is made from high-quality ABS materials. ECE 22.05 homologation ABS shell Micrometric fastening system 2 EPS sizes Emergency cheek pad release\r\n\r\nHomologacja ECE 22.05\r\nSkorupa ABS\r\nZapiêcie mikrometryczne\r\n2 wielkoœci EPS\r\nAwaryjne wypiêcie poduszek policzkowych.", ProductCategory = helmet, Supplier = imx });
+            productDataStore.Add(new Product { Name = "HJC Helmet", DefaultPrice = 179.0m, Currency = "USD", Description = "Just a normal helmet, gives a certain chance to safe the user from certain death", ProductCategory = helmet, Supplier = hjc });
+            productDataStore.Add(new Product { Name = "Broozer", DefaultPrice = 479.0m, Currency = "USD", Description = "Jaw Helmet BELL BROOZER SOLID MATTE BLACK", ProductCategory = helmet, Supplier = bell });
+            productDataStore.Add(new Product { Name = "BROGER MONTANA BLACK", DefaultPrice = 89.0m, Currency = "USD", Description = "Jacket BROGER MONTANA BLACK", ProductCategory = jackets, Supplier = broger });
+            productDataStore.Add(new Product { Name = "Helmy helmet", DefaultPrice = 89.0m, Currency = "USD", Description = "Protects your skull and brain", ProductCategory = helmet, Supplier = bell });
+            productDataStore.Add(new Product { Name = "Shima Giro 2.0", DefaultPrice = 89.0m, Currency = "USD", Description = "Tactical pants in safe, motocyclical edition is model Shima Giro 2.0\r\n", ProductCategory = pants, Supplier = giro });
+            productDataStore.Add(new Product { Name = "Gogles", DefaultPrice = 289.0m, Currency = "USD", Description = "Protects your face and eyes, you can stay beautiful for longer\r\n", ProductCategory = googgles, Supplier = gogler });
         }
     }
 }
--- src/Codecool.CodecoolShop/wwwroot/js/site.js
+++ src/Codecool.CodecoolShop/wwwroot/js/site.js
@@ -29,3 +29,4 @@ function toggleDescription(productId) {
     fullDescription.style.display = "inline";
     buttHide.style.display = "none";
 }
+
